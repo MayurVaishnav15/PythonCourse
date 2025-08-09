0b83cf400d67241c0df0e18e10bbdad5a1b7527c
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-09 13:01:24 +0530

    typecasting learning

---
+++ 04_typecasting/01_typecasting.py
@@ -0,0 +1,20 @@
+a = 22
+print(a)
+print(type(a))
+d = 333
+
+b = "22"
+print(b)
+print(type(b))
+
+print("hello world")
+
+# converting b to an  integer 
+c = int(b)
+print(c)
+print(type(c))
+
+print(type(d))
+e = str(d)
+print(e)
+print(type(e))
