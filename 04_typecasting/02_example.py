304f922541c25aeae8d1d5d8194ebc9547aae755
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-09 17:30:57 +0530

    typecasting example

---
+++ 04_typecasting/02_example.py
@@ -0,0 +1,14 @@
+num_int = 22
+num_str = str(num_int)
+print(num_str)
+print(type(num_str))
+
+num_str = "33"
+num_int = int(num_str)
+print(num_int)
+print(type(num_int))
+
+pi_num = 3.14
+int_num = int(pi_num)
+print(int_num)
+print(type(int_num))
