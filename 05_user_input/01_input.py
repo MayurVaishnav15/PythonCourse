871ea71be6085a8d79cfb3497bd81b08a4a44946
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-09 18:18:14 +0530

    input taking

--- 05_user_input/01_input.py
+++ 05_user_input/01_input.py
@@ -1,4 +1,4 @@
-a = int(input("Enter a  number : "))
+a = int(input("Enter a  number : "))       #typecasting is done to int
 b = int(input("Enter second number : "))
 print("1st number is ",a)
 print("2nd number is ",b)
