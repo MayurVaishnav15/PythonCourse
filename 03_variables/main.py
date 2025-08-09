4d18db949bf89aeeaa5b844bfda17d424f42db73
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-09 12:58:43 +0530

    learning variables

---
+++ 03_variables/main.py
@@ -0,0 +1,26 @@
+age = 22            #int
+name = "mayur"      #string
+cgpa = 8.5          #float
+
+#  python is dynamically typed language means it doesn't
+#  need to be explicitly specified the data type
+
+age = "22"
+print("Age is : "+age)
+print(type(age))
+
+cgpa = "8.5"
+print("The CGPA is : "+cgpa)
+print(type(cgpa))
+
+name = "Mayur"
+print("Name is : "+name)
+print(type(name))
+
+percentage = 85
+print(percentage)
+print(type(percentage))
+
+is_completed = True
+print(is_completed)
+print(type(is_completed))
