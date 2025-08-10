c31ecfec55b6ae916d8305a3031c6d18df165ba0
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-10 21:00:56 +0530

    operators in python

---
+++ 07_operators/01_operators.py
@@ -0,0 +1,11 @@
+a = 5
+
+b = 2
+
+# Arithmetic Operators in Python
+print(" a + b = ", a+b)
+print(" a - b = ", a-b)
+print(" a * b = ", a*b)
+print(" a / b = ", a/b)
+print(" a // b = ", a//b)
+print(" a ** b = ", a**b)
