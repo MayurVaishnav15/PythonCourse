ed40adf112fe0935fdf4f638ab7357cc25d80dec
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-10 20:41:17 +0530

    escape symbols like \

---
+++ 06_comments_print_esc/Escape.py
@@ -0,0 +1,5 @@
+print("hello world \nhello mayur") # use \n for next line in same print
+print ("hello world // \\")        # use double back slash if you want to print single back slash 
+print("hello world \" ")           # use back slash and then double quote if we want to print a double quote under a double quote print statement 
+print ("hello world ' ")           # single quote under the double is okay
+print (' hello world \' ')         # same way this is also okay 
