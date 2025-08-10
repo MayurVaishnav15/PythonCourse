da6fe80835d40f0ebb3955c850e41f4786b1f146
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-10 22:03:49 +0530

    if elif else

---
+++ 08_conditionals/if_elif_else.py
@@ -0,0 +1,8 @@
+age = int(input("Enter Your Age : "))
+
+if(age>18):
+    print("You can drive...")
+elif(age==18):
+    print("Lets Schedule an interview...")
+else:
+    print("You cannot drive...")
