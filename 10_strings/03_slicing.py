2335d6589f3410713913c6d9509d93584a614f9e
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-11 20:39:26 +0530

    slicing +,-,skip

--- 10_strings/03_slicing.py
+++ 10_strings/03_slicing.py
@@ -6,3 +6,8 @@ print(name[0:-1]) #this will be taken as 0 to (5-1) that is 4 ,,so [0:4]
 
  #skip characters slicing:-
 print(name[0:8:2]) #this last two means skip one character 
+
+
+# if some numbers are not available
+print(name[:4]) #it takes first empty as zero always i.e. [0:4]
+print(name[0:]) #it takes the second empty as length full i.e. [1:14]
