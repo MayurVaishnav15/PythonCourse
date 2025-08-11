84c4e0ba2becbd86d7ebc6d59b91874e650d9c9b
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-11 14:01:06 +0530

    indexing the strings

---
+++ 10_strings/02_indexing.py
@@ -0,0 +1,12 @@
+name = "Mayur"
+print(name[3])
+print(name[-1])
+
+
+""" concept   M   a   y   u   r 
+              1   2   3   4   5
+             -5  -4  -3  -2  -1   """
+
+
+"""            what to do when a negative index is given :-      
+               just add the original string length to it                  """
