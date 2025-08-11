4a68a6c52fdbd222e5b95cfe9576d8d67d794088
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-11 20:33:50 +0530

    slicing +,-,skip

---
+++ 10_strings/03_slicing.py
@@ -0,0 +1,8 @@
+name = "mayur123456789"
+print(name[0:3])
+print(name[0:-1]) #this will be taken as 0 to (5-1) that is 4 ,,so [0:4]
+                  # and as they print till n-1 , 0 to 3 will be printed
+
+
+ #skip characters slicing:-
+print(name[0:8:2]) #this last two means skip one character 
