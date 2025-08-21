fcf36a216ca867ef4d96c3481a9d2e9dc0e446e3
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-21 21:51:26 +0530

    use of F strings

--- 10_strings/05_fstring.py
+++ 10_strings/05_fstring.py
@@ -3,8 +3,6 @@ a= "Mayur"
 a1=1000
 s1=template.format(a,a1)
 print(s1)
-
 # or print(template.format(a,a1)) 
-
 # Now use this concept in fstring
 print(f"hey {a}, you are awesome take this {a1}$ bag") 
