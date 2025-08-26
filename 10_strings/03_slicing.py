689d5456027a37cb1702791e7c45725e636bfc70
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-26 09:03:12 +0530

    slicing all types

--- 10_strings/03_slicing.py
+++ 10_strings/03_slicing.py
@@ -1,7 +1,7 @@
 name = "mayur123456789"
 print(name[0:3])
-print(name[0:-1]) #this will be taken as 0 to (5-1) that is 4 ,,so [0:4]
-                  # and as they print till n-1 , 0 to 3 will be printed
+print(name[0:-1]) #this will be taken as 0 to (15-1) that is 4 ,,so [0:4]
+                  # and as they print till n-1 , 0 to 14 will be printed
 
 
  #skip characters slicing:-
