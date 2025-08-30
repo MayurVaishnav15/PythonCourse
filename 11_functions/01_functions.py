d93c72768e2feda52db5826608f85e53f9d9fe17
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-08-30 09:13:55 +0530

    functions intro

---
+++ 11_functions/01_functions.py
@@ -0,0 +1,8 @@
+# Every function is declared with def keyword in python , followed by function name according to task ending by :
+
+def average(a,b,c):
+    d =(a+b+c)/3
+    # print(d)  
+    return d     #None will be printed if we do not  use return function
+o1 = average(3,4,5)
+print(o1)
