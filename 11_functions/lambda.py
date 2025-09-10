9225fa65a0a5b2dc1f0380ef9f9d2a56b8450128
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-09-10 12:39:06 +0530

    lambda functions

---
+++ 11_functions/lambda.py
@@ -0,0 +1,18 @@
+square = lambda X: X*X
+print(square(3))
+"""
+Alternative to this 
+def square(x):
+    return x*x
+"""
+
+sum = lambda x,y: x+y
+print(sum(3,4))
+
+"""
+Alternative to this
+
+def sum(x,y):
+   return x+y
+
+"""
