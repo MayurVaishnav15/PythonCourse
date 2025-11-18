2a7eb771f81042caba9ab7bf3783214a8481e299
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-11-18 09:23:06 +0530

    indexing + & - in python

--- 10_strings/02_indexing.py
+++ 10_strings/02_indexing.py
@@ -1,12 +1,16 @@
 name = "Mayur"
-print(name[3])
+print(name[4])   
 print(name[-1])
 
 
 """ concept   M   a   y   u   r 
-              1   2   3   4   5
+              0   1   2   3   4   
              -5  -4  -3  -2  -1   """
 
 
 """            what to do when a negative index is given :-      
-               just add the original string length to it                  """
+               just add the original string length to it  , 
+               Original means count starting by 1 not 0     
+               For above string the max length is 5 
+                                    max index length is 4
+                                    and max neg index is -5           """
