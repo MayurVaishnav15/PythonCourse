a31b27159afe7239e7bcf09c587d2bce79a39787
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-12-24 19:37:13 +0530

    methods in list

--- 12_dataStractures/02_list_methods.py
+++ 12_dataStractures/02_list_methods.py
@@ -6,3 +6,21 @@ marks.pop()
 print(marks)
 marks.insert(1,'33','11')
 print(marks)
+
+
+marks = [13, 55, 64,99,]
+mixed = ["34", "hello", "3","2", "False","?","/"]
+extra_marks = [33,55,66,65]
+
+print(marks[2:4])
+print(marks[2])
+print(mixed[3:6]) 
+# print(mixed.insert(2,[2,3,4]))    This line return none because List.insert() do not return any value.
+# mixed.insert(2,[2,3,4])
+print(mixed)  
+marks.extend(extra_marks)    # Adds extra_marks list to marks list
+print(marks)
+marks.sort()
+print(marks)
+mixed.sort()
+print(mixed)
