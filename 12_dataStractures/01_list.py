e956742c61b211f72844b8df1ffae537d13fdecb
MayurVaishnav15 <mayurvaishnav1523@gmail.com>
2025-12-24 19:36:09 +0530

    list in python

--- 12_dataStractures/01_list.py
+++ 12_dataStractures/01_list.py
@@ -1,6 +1,7 @@
-marks = [13, 55, 64,"99", [88]]
-mixed = [34, "hello", 3,2, False, [2,3,5]]
+marks = [13, 55, 64,99,]
+mixed = ["34", "hello", "3","2", "False","?","/"]
+extra_marks = [33,55,66,65]
 
 print(marks[2:4])
 print(marks[2])
-print(mixed[3:6])                               
+print(mixed[3:6]) 
